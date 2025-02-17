5ac06791ecd4432dc090fd81b7e9817f4cd3e176
yifufang <yffang01@gmail.com>
2025-02-16 19:25:54 -0800

    expense database and apis

--- backend/src/main/java/com/cmpe295/backend/repository/expenseRepository.java
+++ backend/src/main/java/com/cmpe295/backend/repository/expenseRepository.java
@@ -1,11 +1,10 @@
 package com.cmpe295.backend.repository;
 
-import com.cmpe295.backend.model.User;
+import com.cmpe295.backend.model.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface expenseRepository extends JpaRepository<Expense, Integer> {
     // Additional query methods can go here
 }
-
