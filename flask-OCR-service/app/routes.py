3ba89bd962d934dd6757fd8f78ff987105ab574a
yifufang <yffang01@gmail.com>
2025-04-23 02:26:26 -0700

    celery

--- flask-OCR-service/app/routes.py
+++ flask-OCR-service/app/routes.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify, Blueprint, current_app
 from .logger_utils import info, error  # Replace direct logger usage
+from PIL import Image
+from io import BytesIO
+import pytesseract
 #------tasks--------
 from .task import process_image
 
@@ -20,8 +23,27 @@ def accept_image():
     # Process the image upload
     info("Image received and starting text extraction with pytesseract...")
     info(f"Request content type: {request.content_type}")
-    
-    data = process_image.delay(request)  # Call the Celery task asynchronously
-    # Send the result to Django backend API at route process_text_from_flask
 
-    return jsonify({"data": data})
+    if 'image' in request.files:
+        info("Processing file from form-data")
+        file = request.files['image']
+        image_bytes = file.read()
+    elif request.is_json and 'image' in request.json:
+        info("Processing image from JSON payload as byte array")
+        # Process image sent as byte array in JSON payload instead of file upload
+        byte_array = request.json['image']  # list of ints
+        image_bytes = bytes(byte_array)      # converting list of ints to bytes
+        # Convert byte array to PIL Image
+        """ image = Image.open(BytesIO(image_bytes)) """
+        # check image opened
+        if image_bytes is None:
+            error("Failed to open image from byte array")
+    else:
+        error("No valid image provided in either form-data or JSON payload")
+    task = process_image.delay(image_bytes)  # Call the Celery task asynchronously
+    extracted_text = task.get(timeout=30)
+    print(f"Extracted text: {extracted_text}")
+    # Send the result to Django backend API at route process_text_from_flask
+    if extracted_text is None or not extracted_text.strip():
+        extracted_text = "No text found in the image."
+    return jsonify({"sender": "bot", "text": extracted_text})
--- flask-OCR-service/app/task.py
+++ flask-OCR-service/app/task.py
@@ -6,29 +6,18 @@ from io import BytesIO
 
 
 @shared_task
-def process_image(request):
+def process_image(image_bytes):
     """
     Process the image bytes and extract text using pytesseract.
     """
-    if 'image' in request.files:
-        info("Processing file from form-data")
-        file = request.files['image']
-        image = Image.open(file.stream)  # Open the uploaded image using Pillow
-    elif request.is_json and 'image' in request.json:
-        info("Processing image from JSON payload as byte array")
-        # Process image sent as byte array in JSON payload instead of file upload
-        byte_array = request.json['image']  # list of ints
-        image_bytes = bytes(byte_array)      # converting list of ints to bytes
+    try:
+        # Convert byte array to PIL Image
         image = Image.open(BytesIO(image_bytes))
-        # check image opened
-        if image is None:
-            error("Failed to open image from byte array")
-    else:
-        error("No valid image provided in either form-data or JSON payload")
-
-    extracted_text = pytesseract.image_to_string(image)
-    if not extracted_text:
-        extracted_text = "No text found in the image."
-    info(f"Extracted text: {extracted_text}")
-    data = [{"sender": "bot", "text": extracted_text}]
-    return data
+        extracted_text = pytesseract.image_to_string(image)
+        info(f"Extracted text: {extracted_text}")
+        return extracted_text
+    except Exception as e:
+        error(f"Error processing image: {e}")
+        extracted_text = None
+        return None
+    
